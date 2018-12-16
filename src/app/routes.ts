1f8df8a499a2848d4f595ef4fa3495bea9e192b4
thomaskleist <thomas@kleist-it.com>
2018-12-16 20:45:28 +0100

    added sitenav
    Signed-off-by: thomaskleist <thomas@kleist-it.com>

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,12 +2,15 @@ import {Routes} from '@angular/router';
 import {StamBladComponent} from './stam-blad/stam-blad.component';
 import {IndexComponent} from './index/index.component';
 import {IsUSerGuard} from './is-user.guard';
+import {MediaPlanComponent} from './media-plan/media-plan.component';
 
 export const routes: Routes = [
-  {path: '' , component: IndexComponent},
-  {path: 'stamblad', component: StamBladComponent}
+  {path: '', component: IndexComponent},
+  {path: 'stamblad', component: StamBladComponent},
+  {path: 'medieplan', component: MediaPlanComponent},
+  
 
-]
+];
 
 
 
